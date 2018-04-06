cbd0c56361ccedf9ee9474cbdada46a734fd2c19
bowenh94 <hbw895@gmail.com>
2018-04-06 15:45:38 +1000

    structure v1.0

--- src/Server/ConsensusModule.java
+++ src/Server/ConsensusModule.java
@@ -3,9 +3,13 @@ package Server;
 public class ConsensusModule {
 	private Log log;
 	private StateMachine stateMachine;
+	private String state = "Follower";
 	public ConsensusModule() {
 		// TODO Auto-generated constructor stub
 		this.log = new Log();
 		this.stateMachine = new StateMachine();
+	}
+	public String getState() {
+		return this.state;
 	}
 }
