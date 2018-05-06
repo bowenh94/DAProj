12ef3c63b7d377c6ff508511a4210570d28ec2ab
JinchaoCai <JinchaoCai@192.168.1.7>
2018-05-06 10:26:31 +1000

    first version of follower role

--- src/Server/FollowerCM.java
+++ src/Server/FollowerCM.java
@@ -1,11 +1,39 @@
 package Server;
 
+import java.util.Timer;
+import java.util.Random;
+import java.util.TimerTask;
+
 public class FollowerCM extends ConsensusModule{
 
+	private Timer timeoutTimer;
+	
+	private void resetTimeoutTimer() {
+		timeoutTimer.cancel();
+		Random random = new Random();
+	}
+	
+	private Timer configTimer(long milli) {
+		Timer timer = new Timer(false);
+		TimerTask timerTask = new TimerTask() {
+			public void run() {
+				FollowerCM.this.handleTimeout();
+			}
+		};
+		timer.schedule(timerTask, milli);
+		return timer;
+	}
+	
+	private void handleTimeout() {
+		timeoutTimer.cancel();
+		ConsensusModule.startMode(new CandidateCM());
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		Random random = new Random();
+		timeoutTimer = configTimer(random.nextInt(1000));
 	}
 
 	@Override
