10e9752c7adf442a7e1414463f772274437762ad
OwenZhu <398094982@qq.com>
2018-05-10 20:00:43 +1000

    update server

--- src/Server/FollowerCM.java
+++ src/Server/FollowerCM.java
@@ -4,49 +4,56 @@ import java.util.Timer;
 import java.util.LinkedList;
 import java.util.Random;
 
-public class FollowerCM extends ConsensusModule{
+public class FollowerCM extends ConsensusModule {
 
 	private Timer timeoutTimer;
 	private int TIMEOUT_TIMER_ID = 1;
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		synchronized (cmLock) {
+			System.out.println("S" + cmServerId + "." + cmDAServer.getCurrentTerm() + ": switched to follower mode.");
+
+			// Create timer to detect missing leader
+			Random rand = new Random();
+			timeoutTimer = scheduleTimer(
+					rand.nextInt(this.ELECTION_TIMEOUT_MAX - this.ELECTION_TIMEOUT_MIN) + this.ELECTION_TIMEOUT_MIN,
+					this.TIMEOUT_TIMER_ID); // may need to change timer id here
+		}
 	}
 
 	@Override
 	public int appendEntries(int leaderTerm, int leaderID, int prevLogIndex, int prevLogTerm, Entry[] entries,
 			int leaderCommit) {
 		// TODO Auto-generated method stub
-		synchronized(cmLock) {
+		synchronized (cmLock) {
 			System.out.println("Server " + cmDAServer.getServerId());
 			this.resetTimeoutTimer();
 			int term = cmDAServer.getCurrentTerm();
-			if(leaderTerm>=term) {
+			if (leaderTerm >= term) {
 				cmDAServer.setCurrentTerm(term);
 			}
-			int termAtLastIndex=log.getLastTerm();
-			if(termAtLastIndex==prevLogTerm) {
-				if(prevLogIndex==(log.get().size()-1)) {
-					for(Entry entry : entries) {
+			int termAtLastIndex = log.getLastTerm();
+			if (termAtLastIndex == prevLogTerm) {
+				if (prevLogIndex == (log.get().size() - 1)) {
+					for (Entry entry : entries) {
 						log.get().add(entry);
 					}
-				}
-				else if(entries==null) {
+				} else if (entries == null) {
 					return -1;
-				}
-				else if((prevLogIndex == -1) || ((log.getEntry(prevLogIndex) != null) && (log.getLastTerm() == prevLogTerm))){
-					LinkedList<Entry> tmpEntries = new LinkedList<Entry> ();
-					for (int i=0; i<=prevLogIndex; i++) {
-					  Entry entry = log.getEntry(i);
-					  tmpEntries.add (entry);
+				} else if ((prevLogIndex == -1)
+						|| ((log.getEntry(prevLogIndex) != null) && (log.getLastTerm() == prevLogTerm))) {
+					LinkedList<Entry> tmpEntries = new LinkedList<Entry>();
+					for (int i = 0; i <= prevLogIndex; i++) {
+						Entry entry = log.getEntry(i);
+						tmpEntries.add(entry);
 					}
-					for(Entry entry : entries) {
+					for (Entry entry : entries) {
 						tmpEntries.add(entry);
 					}
 					log.setLog(tmpEntries);
-				}
-				else {
+				} else {
 					return -1;
 				}
 			}
@@ -57,14 +64,14 @@ public class FollowerCM extends ConsensusModule{
 	@Override
 	public int requestVote(int candidateTerm, int candidateID, int lastLogIndex, int lastLogTerm) {
 		// TODO Auto-generated method stub
-		synchronized(cmLock) {
+		synchronized (cmLock) {
 			int term = cmDAServer.getCurrentTerm();
-			if(candidateTerm>=term && cmDAServer.getServerId()==0 && lastLogIndex>=cmLastCommitId) {
-				System.out.println("Server " + cmServerId + "received vote request from server " + candidateID +" and vote");
+			if (candidateTerm >= term && cmDAServer.getServerId() == 0 && lastLogIndex >= cmLastCommitId) {
+				System.out.println(
+						"Server " + cmServerId + "received vote request from server " + candidateID + " and vote");
 				cmDAServer.setCurrentTerm(candidateTerm);
 				return 0;
-			}
-			else {
+			} else {
 				cmDAServer.setCurrentTerm(candidateTerm);
 				return term;
 			}
@@ -74,19 +81,21 @@ public class FollowerCM extends ConsensusModule{
 	@Override
 	protected void handleTimeout(int timerId) {
 		// TODO Auto-generated method stub
-		synchronized(cmLock) {
-			if(timerId == this.TIMEOUT_TIMER_ID) {
+		synchronized (cmLock) {
+			if (timerId == this.TIMEOUT_TIMER_ID) {
 				timeoutTimer.cancel();
 				System.out.println(cmServerId + " has not received heartbeat from leader");
 				RPCImpl.startMode(new CandidateCM());
 			}
 		}
 	}
-	
+
 	private void resetTimeoutTimer() {
 		timeoutTimer.cancel();
 		Random random = new Random();
-		timeoutTimer = scheduleTimer(random.nextInt(this.ELECTION_TIMEOUT_MAX - this.ELECTION_TIMEOUT_MIN) + this.ELECTION_TIMEOUT_MIN, this.TIMEOUT_TIMER_ID);
+		timeoutTimer = scheduleTimer(
+				random.nextInt(this.ELECTION_TIMEOUT_MAX - this.ELECTION_TIMEOUT_MIN) + this.ELECTION_TIMEOUT_MIN,
+				this.TIMEOUT_TIMER_ID);
 	}
-	
+
 }
