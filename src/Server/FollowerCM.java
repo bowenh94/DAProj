4dfc3d012f17562987e5d89b7e47a60006561f56
OwenZhu <398094982@qq.com>
2018-05-14 00:55:39 +1000

    modify follower mode, fixed bugs in appendEntries (still need to check)

--- src/Server/FollowerCM.java
+++ src/Server/FollowerCM.java
@@ -31,24 +31,52 @@ public class FollowerCM extends ConsensusModule {
 			int leaderCommit) {
 		// TODO Auto-generated method stub
 		synchronized (cmLock) {
-			System.out.println("S"+newServer.serverId + "."+newServer.currentTerm +" Leader term is "+leaderTerm + " with Entry of ");
+			System.out.println("S" + newServer.serverId + "." + newServer.currentTerm + " Leader term is " + leaderTerm
+					+ " with Entry of ");
 
 			this.resetTimeoutTimer();
 			int term = newServer.currentTerm;
 			if (leaderTerm >= term) {
 				newServer.setCurrentTerm(leaderTerm);
-				//newServer.votedFor = -1;
-			}
-			int termAtIndex = newServer.log.getEntry(prevLogIndex).getTerm();
-			if (termAtIndex == prevLogTerm) {
-				Entry[] newEntries = newServer.stringtoEntries(entries);
-				newServer.log.insert(newEntries, prevLogIndex, prevLogTerm);		
-				System.out.println("Return 0 here -----------------------------------------");
-				return 0;
+				// newServer.votedFor = -1;
 			} else {
-				System.out.println("Return -1 here ----------------------------------------");
+				// condition 1: Reply false if leader term < currentTerm
 				return -1;
 			}
+
+			/*
+			 * condition 2: Reply false if log doesn¡¯t contain an entry at prevLogIndex
+			 * whose term matches prevLogTerm
+			 */
+			Entry entryAtIndex = newServer.log.getEntry(prevLogIndex);
+			if (entryAtIndex == null || entryAtIndex.getTerm() != prevLogTerm) {
+				return -1;
+			}
+
+			// TODO: please check condition 3 & 4 CAREFULLY. Fukcing too tired, need sleep (Owen)
+			/*
+			 * condition 3: If an existing entry conflicts with a new one (same index but
+			 * different terms), delete the existing entry and all that follow it
+			 */
+
+			/* condition 4: Append any new entries not already in the log */
+			Entry[] newEntries = newServer.stringtoEntries(entries);
+			newServer.log.insert(newEntries, prevLogIndex, prevLogTerm);
+			
+			/*
+			 * condition 5: If leaderCommit > commitIndex, set commitIndex =
+			 * min(leaderCommit, index of last new entry)
+			 */
+			if (leaderCommit > ConsensusModule.cmLastCommitId) {
+				if (leaderCommit > newServer.log.getLastIndex()) {
+					ConsensusModule.cmLastCommitId =  newServer.log.getLastIndex();
+				}
+				else{
+					ConsensusModule.cmLastCommitId = leaderCommit;
+				}
+			}
+			
+			return 0;
 		}
 	}
 
@@ -58,9 +86,9 @@ public class FollowerCM extends ConsensusModule {
 		synchronized (cmLock) {
 			int term = newServer.currentTerm;
 			if (candidateTerm >= term && newServer.votedFor == -1 && lastLogIndex >= cmLastCommitId) {
-				System.out.println(
-						"Server " + newServer.serverId + "received vote request from server " + candidateID + " and vote");
-				//newServer.setCurrentTerm(candidateTerm);
+				System.out.println("Server " + newServer.serverId + "received vote request from server " + candidateID
+						+ " and vote");
+				// newServer.setCurrentTerm(candidateTerm);
 				newServer.votedFor = candidateID;
 				return 0;
 			} else {
